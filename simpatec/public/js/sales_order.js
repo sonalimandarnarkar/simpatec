0fc2d656acac4e7cef9c8804c634822072badbd4
beingeek <furqan.790@outlook.com>
2023-12-05 13:51:03 +0000

    fix: reverting Follow Up Maintenance case

--- simpatec/public/js/sales_order.js
+++ simpatec/public/js/sales_order.js
@@ -8,19 +8,5 @@ frappe.ui.form.on('Sales Order', {
         		})                
             }, __("Create"));
         }
-
-        frm.events.toggle_software_maintenance_reqd(frm);
-    },
-
-    sales_order_type(frm) {
-        frm.events.toggle_software_maintenance_reqd(frm);
-    },
-
-    toggle_software_maintenance_reqd(frm) {
-        if (frm.doc.sales_order_type == 'Follow Up Maintenance') {
-            frm.set_df_property('software_maintenance', 'reqd', true);
-        } else {
-            frm.set_df_property('software_maintenance', 'reqd', false);
-        }
     }
 })
