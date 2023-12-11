dfc352274affe85aaacd7a1e1363f37a4c9bd6bd
beingeek <furqan.790@outlook.com>
2023-12-11 11:13:44 +0000

    fix: Message update in Software Maintenance

--- simpatec/simpatec/doctype/software_maintenance/software_maintenance.py
+++ simpatec/simpatec/doctype/software_maintenance/software_maintenance.py
@@ -13,10 +13,8 @@ class SoftwareMaintenance(Document):
 		if self.sales_order:
 			software_maintenance = frappe.get_cached_value('Sales Order', self.sales_order, 'software_maintenance')
 			if software_maintenance and software_maintenance != self.name:
-				frappe.throw(_('{0} already exist against {1}').format(
-					frappe.get_desk_link("Software Maintenance", software_maintenance), 
-					frappe.get_desk_link("Sales Order", self.sales_order)))
+				frappe.throw(_('Software Maintenance already exist for {0}').format(frappe.get_desk_link("Sales Order", self.sales_order)))
 			
 			if not software_maintenance:
 				frappe.db.set_value('Sales Order', self.sales_order, 'software_maintenance', self.name)
-				frappe.msgprint(_('{0} updated').format(frappe.get_desk_link("Sales Order", self.sales_order)))
+				frappe.msgprint(_('"Software Maintenance" field updated in {0}').format(frappe.get_desk_link("Sales Order", self.sales_order)))
