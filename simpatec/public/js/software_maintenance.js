190b0f63bc8fd97743c81e4fc0398fe01e6623d0
beingeek <furqan.790@outlook.com>
2024-01-16 11:23:27 +0000

    chore: cleanup

--- simpatec/public/js/software_maintenance.js
+++ simpatec/public/js/software_maintenance.js
@@ -1,8 +1,6 @@
 frappe.ui.form.on('Software Maintenance', {
     refresh(frm) {
         frm.add_custom_button('Software Maintenance', function () { 
-            debugger;
-
             frappe.call({
                 method: "simpatec.events.sales_order.make_sales_order",
                 args: {
@@ -13,30 +11,6 @@ frappe.ui.form.on('Software Maintenance', {
                     frm.set_value("total_subscribers", r.message);
                 },
             });
-
-            // frappe.prompt(
-            //     {
-            //         fieldtype: "Date",
-            //         label: __("Trigger Date"),
-            //         fieldname: "trigger_date",
-            //         reqd: 1,
-            //     },
-            //     function (data) {
-            //         frappe.call({
-            //             method: "simpatec.events.sales_order.make_sales_order",
-            //             args: {
-            //                 software_maintenance: frm.doc.name,
-            //                 // trigger_date: data.trigger_date,
-            //             },
-            //             callback: function (r) {
-            //                 frm.set_value("total_subscribers", r.message);
-            //             },
-            //         });
-            //     },
-            //     __("Import Subscribers"),
-            //     __("Import")
-            // );
-            
         }, __("Create Sales Order"));
     }
 })
