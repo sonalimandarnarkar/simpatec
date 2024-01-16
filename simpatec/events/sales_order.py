51d0ee38388d3a87c9ecccddc7f341193ea933fe
beingeek <furqan.790@outlook.com>
2024-01-16 11:13:06 +0000

    fix: gap year extra day issue

--- simpatec/events/sales_order.py
+++ simpatec/events/sales_order.py
@@ -1,6 +1,6 @@
 import frappe
 from frappe import _
-from frappe.utils import cint, cint, flt, add_days, add_years, today
+from frappe.utils import cint, cstr, flt, add_days, add_years, today, getdate
 from frappe.model.mapper import get_mapped_doc
 
 
@@ -76,6 +76,7 @@ def create_followup_software_maintenance_sales_order(date=None):
 			frappe.db.commit()
 
 
+@frappe.whitelist()
 def make_sales_order(software_maintenance, debug=False):
 	software_maintenance = frappe.get_doc("Software Maintenance", software_maintenance)
 	if not software_maintenance.assign_to:
@@ -85,16 +86,26 @@ def make_sales_order(software_maintenance, debug=False):
 	if not employee:
 		frappe.throw(_("User {0} not set in Employee").format(software_maintenance.assign_to))
 
+	performance_period_start = add_days(software_maintenance.performance_period_end, 1)
+	performance_period_end = add_years(performance_period_start, software_maintenance.maintenance_duration)
+	total_days = getdate(performance_period_end) - getdate(performance_period_start)
+
+	days_diff = total_days.days%365
+	if days_diff != 0:
+		performance_period_end = add_days(performance_period_end, -days_diff)
+		total_days = getdate(performance_period_end) - getdate(performance_period_start)
+
+	transaction_date = add_days(performance_period_end, -cint(software_maintenance.lead_time))
 	sales_order = frappe.new_doc("Sales Order")
 	sales_order.customer_subsidiary = software_maintenance.customer_subsidiary
-	sales_order.performance_period_start = add_days(software_maintenance.performance_period_end, 1)
-	sales_order.performance_period_end = add_years(sales_order.performance_period_start, software_maintenance.maintenance_duration)
+	sales_order.performance_period_start = performance_period_start
+	sales_order.performance_period_end = performance_period_end
 	sales_order.software_maintenance = software_maintenance.name
 	sales_order.item_group = software_maintenance.item_group
 	sales_order.customer = software_maintenance.customer
 	sales_order.sales_order_type = "Follow Up Maintenance"
 	sales_order.ihr_ansprechpartner = employee
-	sales_order.transaction_date = add_days(software_maintenance.performance_period_end, -cint(software_maintenance.lead_time))
+	sales_order.transaction_date = transaction_date
 	sales_order.order_type = "Sales"
 
 	for item in software_maintenance.items:
