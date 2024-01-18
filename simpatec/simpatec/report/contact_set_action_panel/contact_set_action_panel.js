b95081bf1e82ca2e1e8e489efe8605d4d7796c0d
beingeek <furqan.790@outlook.com>
2024-01-18 15:21:48 +0000

    feat: new report  contact set action panel

---
+++ simpatec/simpatec/report/contact_set_action_panel/contact_set_action_panel.js
@@ -0,0 +1,112 @@
+// Copyright (c) 2024, Phamos GmbH and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+frappe.provide("contact_set_control_panel")
+
+frappe.query_reports["Contact Set Action Panel"] = {
+	"filters": [
+		{
+			"fieldname":"contact_set",
+			"label": __("Contact Set"),
+			"fieldtype": "Link",
+			"options": "Contact Set",
+			"reqd": 1
+		},
+	],
+
+	onload: async function (report) {
+		$(".custom-actions").hide();
+		$(".standard-actions").hide();
+
+		contact_set_control_panel.open_dialog = function (contact_set, contact_set_row, contact_name, notes, status, emails, phone_nos) {
+			var getContactInfoHtml = function (contactInfo, field, linkType, label) {
+				let contactInfoHtmlOutput = '';
+				let html_segment = "<span></span>"
+
+				let linkPrefix = (linkType === 'email') ? 'mailto:' : 'tel:';			
+				contactInfo.forEach(infoObj => {
+					let value = infoObj[field.toLowerCase()];
+					let link = `${linkPrefix}${value}`;					
+					let divElement = `<p><a href="${link}">${value}</a></p>`;			
+					contactInfoHtmlOutput += divElement;
+				});
+
+				if (contactInfoHtmlOutput) {
+					html_segment =  `<div class="form-group">
+						<div class="clearfix">
+							<label class="control-label" style="padding-right: 0px;">${label}</label>
+							<span class="help"></span>
+						</div>
+						<div class="like-disabled-input">${contactInfoHtmlOutput}</div>
+					</div>`
+				}
+				return html_segment
+			}
+
+			let d = new frappe.ui.Dialog({
+				title: "Take Action",
+				// size: "large",
+				 size: "extra-large",
+				fields: [
+					{
+						label: "Contact Name",
+						fieldname: "contact_name",
+						fieldtype: "HTML",
+						options: `<h3>${contact_name}</h3>`,
+						read_only: 1
+					},
+					{
+						label: "Email Address",
+						fieldname: "email_address",
+						fieldtype: "HTML",
+						options: getContactInfoHtml(emails, 'email_id', 'email', "Emails"),
+						hidden: 1
+					},
+					{
+						label: "Phone Nos",
+						fieldname: "phone_nos",
+						fieldtype: "HTML",
+						options: getContactInfoHtml(phone_nos, 'phone', 'phone', "Phone Nos")
+					},
+					{
+						fieldname: "colbreak1234",
+						fieldtype: "Column Break"
+					},
+					{
+						label: "Status",
+						fieldname: "status",
+						fieldtype: "Select",
+						options: "\nNew\nIn Work\nRejected\nOpportunity",
+						default: status
+					},
+					{
+						label: "Notes",
+						fieldname: "notes",
+						fieldtype: "Small Text",
+						default: notes
+					},
+
+				],
+				primary_action_label: "Update",
+				primary_action() {
+					var data = d.get_values();
+					console.log(emails)
+					frappe.call({
+						method: "simpatec.simpatec.report.contact_set_action_panel.contact_set_action_panel.update_row_in_contact_set",
+						args: {
+							contact_set: contact_set,
+							contact_set_row: contact_set_row,
+							data: data
+						},
+						callback: function (r) {
+							report.refresh();
+						}
+					})
+
+					d.hide();
+				}
+			});
+			d.show();
+		}
+	}
+};
