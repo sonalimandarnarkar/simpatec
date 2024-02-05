288f8d69d1d23760137fc932564691c2134f2240
beingeek <furqan.790@outlook.com>
2024-02-05 04:15:33 +0000

    fix: introduced paging mechanism in report to make slow and controlled loading

--- simpatec/simpatec/report/contact_register/contact_register.js
+++ simpatec/simpatec/report/contact_register/contact_register.js
@@ -12,19 +12,23 @@ frappe.query_reports["Contact Register"] = {
 		if ($('body[data-route="query-report/Contact Register"]').length > 0) {
 			let report_wrapper = $('body[data-route="query-report/Contact Register"]');
 			let page_action_wrapper = report_wrapper.find('div[id="page-query-report"]').find(".page-head-content").find(".page-actions");
-			page_action_wrapper.html(`<button class="btn btn-default btn-sm ellipsis contact-set-route ml-2 mr-2" onclick="bulk_update_contact_set()">Add selected to Contact Set</button>
-			<button class="btn btn-default btn-sm ellipsis contact-set-route" onclick="route_to_contact_set()">Switch to Contact Set</button>
-			<div class="filter-selector">
-				<button class="btn btn-default btn-sm filter-button">
-					<span class="filter-icon">
-						${frappe.utils.icon("filter")}
-					</span>
-					<span class="button-label hidden-xs">
-						${__("Filter")}
-					<span>
-				</button>
-			</div>`);
-
+			page_action_wrapper.html(`
+				<button class="btn btn-default btn-sm ellipsis contact-set-route ml-2 mr-2" onclick="bulk_update_contact_set()">Add selected to Contact Set</button>
+				<button class="btn btn-default btn-sm ellipsis contact-set-route" onclick="route_to_contact_set()">Switch to Contact Set</button>
+				<div class="control-input flex align-center" id="load-more-contacts"></div>
+				<div class="filter-selector">
+					<button class="btn btn-default btn-sm filter-button">
+						<span class="filter-icon">
+							${frappe.utils.icon("filter")}
+						</span>
+						<span class="button-label hidden-xs">
+							${__("Filter")}
+						<span>
+					</button>
+				</div>
+			`);
+			
+			setup_paging_area(page_action_wrapper);
 			make_filter_list(page_action_wrapper);
 		}
 
@@ -78,7 +82,6 @@ function update_bulk_list(contact, contact_row) {
 	let id_contact_row = "#" +  contact_row;
 	$(id_contact_row).on("change",function(){
 		var _val = $(this).is(":checked") ? "checked" : "unchecked";
-		console.log(_val);
 		if (_val == "checked") {
 			// if checked then add object {contact, contact_row} if not existt in array frappe.query_reports["Contact Register"]["bulk_update_rows"]
             let exists = frappe.query_reports["Contact Register"]["bulk_update_rows"].some(item => item.contact === contact && item.contact_row === contact_row);
@@ -107,6 +110,59 @@ function bulk_select_all() {
 			$(".bulk-select-contact-set").prop('checked', false);
 			frappe.query_reports["Contact Register"]["bulk_update_rows"] = [];
 		}
+	});
+}
+
+
+function setup_paging_area(page_action_wrapper) {
+
+	const paging_values = [100, 500];
+
+	this.$paging_area = `<div class="list-paging-area level">
+							<div class="level-left">
+								<div class="btn-group">
+									${paging_values
+										.map(
+											(value) => `
+										<button type="button" class="btn btn-default btn-sm btn-paging"
+											data-value="${value}">
+											${value}
+										</button>
+									`
+										)
+										.join("")}
+								</div>
+							</div>
+							<div class="level-right">
+								<button class="btn btn-default btn-more btn-sm">
+									${__("Load More")}
+								</button>
+							</div>
+						</div>`;
+	page_action_wrapper.find('div[id="load-more-contacts"]').html(this.$paging_area);
+
+	// set default paging btn active
+	page_action_wrapper.find(`.btn-paging[data-value="100"]`).addClass("btn-info");
+	// this.$paging_area.find(`.btn-paging[data-value="${this.page_length}"]`).addClass("btn-info");
+	page_action_wrapper.on("click", ".btn-paging, .btn-more", (e) => {
+		const $this = $(e.currentTarget);
+
+		if ($this.is(".btn-paging")) {
+			// set active button
+			page_action_wrapper.find(".btn-paging").removeClass("btn-info");
+			$this.addClass("btn-info");
+
+			this.start = $this.data().value;
+			this.page_length = this.selected_page_count = $this.data().value;
+		} else if ($this.is(".btn-more")) {
+			this.start = this.start + this.page_length;
+			this.page_length = this.selected_page_count || 20;
+		}
+		let filters = this.filter_list.get_filters();
+		let limit = this.start;
+
+		get_and_render_data(filters, limit);
+		// this.refresh();
 	});
 }
 
@@ -164,24 +220,32 @@ var make_filter_list = function (page_action_wrapper) {
 		default_filters: [],
 		on_change: () => {
 			var filters = this.filter_list.get_filters();
-			frappe.call({
-				method: "simpatec.simpatec.report.contact_register.contact_register.execute",
-				args: {
-					filters: filters
-				},
-				freeze: true,
-				freeze_message: "Loading data..."
-			}).then(r => {
-				if (r.message){
-					$(".report-wrapper").next().hide()
-				}
-				var data = {
-					columns: r.message[0],
-					result: r.message[1]
-				}
-				frappe.query_report.prepare_report_data(data);
-				frappe.query_report.render_datatable()
-			})
+			let limit = 100;
+			get_and_render_data(filters, limit);
 		},
 	});
+}
+
+var get_and_render_data = function(filters, limit){
+	frappe.call({
+		method: "simpatec.simpatec.report.contact_register.contact_register.execute",
+		args: {
+			filters: filters,
+			limit: limit
+		},
+		freeze: true,
+		freeze_message: "Loading data..."
+	}).then(r => {
+		if (r.message){
+			$(".report-wrapper").next().hide()
+		}
+		var data = {
+			columns: r.message[0],
+			result: r.message[1],
+			total_count: r.message[2]
+		}
+		frappe.query_report.prepare_report_data(data);
+		frappe.query_report.render_datatable();
+		frappe.query_report.show_status(data["total_count"]);
+	})
 }
