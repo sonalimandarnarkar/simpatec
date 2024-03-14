5905d192cf0a95106fbeb7845a21285cdd3aab5b
Muhammad Ali <swe.mirza.ali@gmail.com>
2024-03-14 17:04:29 +0000

    Issue#16 Add "SimpaTec Settings" single Page

---
+++ simpatec/simpatec/doctype/simpatec_settings/simpatec_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Phamos GmbH and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('SimpaTec Settings', {
+	// refresh: function(frm) {
+
+	// }
+});
