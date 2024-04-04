6308b7c38da6f77d9bcc5b43ad23f31beaabab1d
lajidey-foss <spryng.managed@gmail.com>
2024-04-04 16:27:24 +0100

    Filter Toggle
    Implementatiion of toggle fuction with filter and filter off icon switching

--- simpatec/public/js/quotation.js
+++ simpatec/public/js/quotation.js
@@ -5,21 +5,35 @@ frappe.ui.form.on('Quotation', {
             if (!field.$clear_icon_appended) {
                 field.$clear_icon_appended = true;
 
-                //&#9705;
-				var $clearIcon = $('<span class="clear-icon" style="cursor: pointer; position: absolute; right: 35px; top: 50%; transform: translateY(-50%); font-size: 15px; width: 18px; height: 18px; line-height: 18px; text-align: center; border-radius: 50%;  color: #1C2126;">&#x2716;</span>');
+				var $clearIcon = $('<span id="xsimpactggleicon" class="clear-icon" style="cursor: pointer; position: absolute; right: 35px; top: 50%; transform: translateY(-50%); font-size: 15px; width: 18px; height: 18px; line-height: 18px; text-align: center; border-radius: 50%;  color: #1C2126;"><svg class="icon "> <use href="#icon-filter-x"></use> </svg> </span>');
                 field.$input.parent().css('position', 'relative'); 
                 field.$input.css('position', 'relative'); 
                 field.$input.after($clearIcon);
 
                 $clearIcon.on('click', function() {
-                    
-					frm.set_query("anschreiben_vorlage", () => {
-						let filters = {};
-						/* return {
-							filters: filters
-						}
-						 */
-					});
+					let qd = cur_frm.fields_dict["anschreiben_vorlage"].get_query();
+					var x = document.getElementById("xsimpactggleicon").children[0];
+					if (!(qd == undefined) && qd.filters.language === frm.doc.language){
+						x.innerHTML = '<use href="#icon-filter"></use>';
+						frm.set_query("anschreiben_vorlage", () => {
+							let filters = {};
+							/* return {
+								filters: filters
+							} */
+						});
+					}
+					else{
+						x.innerHTML = '<use href="#icon-filter-x"></use>';
+						frm.set_query("anschreiben_vorlage", () => {
+							let filters = {};
+							if (frm.doc.language) filters["language"] = frm.doc.language;
+							return {
+								filters: filters
+							}
+						});
+
+					}
+                    					
 					frm.set_value(field.df.fieldname, '');
                 });
             }
