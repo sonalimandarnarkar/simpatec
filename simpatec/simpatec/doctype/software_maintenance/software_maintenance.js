d56193f7069ab386c9bc393a305cede54d580662
Mirza Muhammad Ali Baig <swe.mirza.ali@gmail.com>
2024-06-27 12:16:46 +0000

    Mandatory Dialog

--- simpatec/simpatec/doctype/software_maintenance/software_maintenance.js
+++ simpatec/simpatec/doctype/software_maintenance/software_maintenance.js
@@ -5,20 +5,59 @@ frappe.ui.form.on('Software Maintenance', {
     refresh(frm) {
         var reoccurring_label = __(`${frm.doc.licence_renewal_via} (Reoccurring Maintenance)`)
         frm.add_custom_button(reoccurring_label, function () {
-            frm.events.make_reoccurring(frm)
+            if(frm.doc.licence_renewal_via == "Quotation"){
+                var d = new frappe.ui.Dialog({
+                    title: __('Quotation Mandatory Fields'),
+                    fields: [
+                        {
+                            "label": "Item Group",
+                            "fieldname": "item_group",
+                            "fieldtype": "Link",
+                            "options": "Item Group",
+                            "default": frm.doc.item_group,
+                            "reqd": 1,
+                        },
+                        {
+                            "label": "Quotation Label",
+                            "fieldname": "quotation_label",
+                            "fieldtype": "Link",
+                            "options": "Angebotsvorlage",
+                            "reqd": 1,
+                        },
+                        {
+                            "label": "Ihr Ansprechpartner",
+                            "fieldname": "ihr_ansprechpartner",
+                            "fieldtype": "Link",
+                            "options": "Employee",
+                            "reqd": 1,
+                        },
+
+                    ],
+                    primary_action: function (data) {
+                        debugger;
+                        frm.events.make_reoccurring(frm, data)
+                        d.hide();
+                    },
+                    primary_action_label: __('Create Quotation')
+                });
+                d.show();
+            }else{
+                frm.events.make_reoccurring(frm)
+            }
         }, __("Create"));
 
 
         //hide all + in the connection
         $('.form-documents button').hide();
     },
-    make_reoccurring(frm){
+    make_reoccurring(frm, mandatory_fields){
         frappe.call({
             method: "simpatec.simpatec.doctype.software_maintenance.software_maintenance.make_reoccuring_sales_order",
             args: {
                 software_maintenance: frm.doc.name,
                 is_background_job: 0,
-                licence_renewal_via: frm.doc.licence_renewal_via
+                licence_renewal_via: frm.doc.licence_renewal_via,
+                mandatory_fields: mandatory_fields
             },
             callback: function (r) {
             },
