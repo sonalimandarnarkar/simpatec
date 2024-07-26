ed2bb679909bcd0ddb02ffe0986d1a5921dfaa2c
Mirza Muhammad Ali Baig <swe.mirza.ali@gmail.com>
2024-07-26 20:59:35 +0000

    Reorder fields, Added Desc and Item name fields

--- simpatec/simpatec/doctype/software_maintenance/software_maintenance.py
+++ simpatec/simpatec/doctype/software_maintenance/software_maintenance.py
@@ -91,9 +91,9 @@ def make_reoccuring_sales_order(software_maintenance, licence_renewal_via=None, 
 			"item_name_en": item.item_name_en,
 			"item_name_de": item.item_name_de,
 			"item_name_fr": item.item_name_fr,
-			"item_description_en": item.id_en,
-			"item_description_de": item.id_de,
-			"item_description_fr": item.id_fr,
+			"item_description_en": item.item_description_en,
+			"item_description_de": item.item_description_de,
+			"item_description_fr": item.item_description_fr,
 		})
 
 	reoccurring_order.insert()
